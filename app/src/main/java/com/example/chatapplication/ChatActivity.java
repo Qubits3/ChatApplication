ec05700b3bb323142f0d1ecf3de981e3a48a5dd0
Metin Yıldıran <metin.1464@gmail.com>
2020-01-18 23:51:30 +0300

    Application finished with bunch of bugs. Gotta fix them.

--- app/src/main/java/com/example/chatapplication/ChatActivity.java
+++ app/src/main/java/com/example/chatapplication/ChatActivity.java
@@ -2,6 +2,9 @@ package com.example.chatapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +12,35 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class ChatActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+    EditText messageText;
+
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+
+    private ArrayList<String> chatMessages = new ArrayList<>();  //RecyclerViewAdapter sınıfına paslanacak ArrayList mesajlar buna eklenir
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,7 +74,20 @@ public class ChatActivity extends AppCompatActivity {
         setTitle(R.string.chat);
         setContentView(R.layout.activity_chat);
 
+        messageText = findViewById(R.id.chat_activity_message_text);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerViewAdapter = new RecyclerViewAdapter(chatMessages);
+
+        RecyclerView.LayoutManager recyclerViewManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(recyclerViewManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(recyclerViewAdapter);
+
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
+
+        getData();
     }
 
     @Override
@@ -58,5 +97,64 @@ public class ChatActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
 
+        String messageToSend = messageText.getText().toString();
+
+        UUID uuid = UUID.randomUUID();
+        String uuidString = uuid.toString();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        String userEmail = "";
+        if (user != null) {
+            userEmail = user.getEmail();
+        }
+
+        databaseReference.child("Chats").child(uuidString).child("usermessage").setValue(messageToSend);
+        databaseReference.child("Chats").child(uuidString).child("useremail").setValue(userEmail);
+        databaseReference.child("Chats").child(uuidString).child("userMessageTime").setValue(ServerValue.TIMESTAMP);
+
+        messageText.setText("");
+
+        getData();
+
+    }
+
+    public void getData() {
+
+        DatabaseReference newReference = database.getReference("Chats");
+
+        Query query = newReference.orderByChild("userMessageTime");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //System.out.println("dataSnapshot Children: " + dataSnapshot.getChildren());
+                //System.out.println("dataSnapshot Value: " + dataSnapshot.getValue());
+                //System.out.println("dataSnapshot Key: " + dataSnapshot.getKey());
+
+                chatMessages.clear();
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+
+                    //System.out.println("data value: " + ds.getValue());
+
+                    HashMap<String, String> hashMap = (HashMap<String, String>) ds.getValue();
+                    String userEmail = hashMap.get("useremail");
+                    String userMessage = hashMap.get("usermessage");
+
+                    chatMessages.add(userEmail + ": " + userMessage);
+
+                    recyclerViewAdapter.notifyDataSetChanged();
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
